383646938229641ea94888fdf2ac67be13b47ad0
KevinMendieta <kekox.dev@gmail.com>
2023-12-11 15:16:15 -0500

    Add solution to problem #2

---
+++ src/2023/02/weStartTheFactory.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from "bun:test";
+
+import { manufacture } from "./weStartTheFactory";
+
+describe("we start the factory problem tests", () => {
+	it("should return a number", () => {
+		const result = manufacture(["tren", "oso", "pelota"], "tronesa");
+		expect(result).toBeArray();
+	});
+	it("should pass test 1", () => {
+		const result = manufacture(["tren", "oso", "pelota"], "tronesa");
+		expect(result).toStrictEqual(["tren", "oso"]);
+	});
+	it("should pass test 2", () => {
+		const result = manufacture(["coche", "muñeca", "balon"], "ocmuñalb");
+		expect(result).toStrictEqual([]);
+	});
+	it("should pass test 3", () => {
+		const result = manufacture(["patineta", "robot", "libro"], "nopor");
+		expect(result).toStrictEqual([]);
+	});
+	it("should pass test 4", () => {
+		const result = manufacture([], "letras");
+		expect(result).toStrictEqual([]);
+	});
+});
---
+++ src/2023/02/weStartTheFactory.ts
@@ -0,0 +1,9 @@
+export function manufacture(gifts: string[], materials: string): string[] {
+	const memory = new Set(materials.split(""));
+
+	return gifts.reduce((finalGifts: string[], gift) => {
+		[...gift].every((character) => memory.has(character)) &&
+			finalGifts.push(gift);
+		return finalGifts;
+	}, []);
+}
