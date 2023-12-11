0f269c6b89ade2321e446db594209038e89fee72
KevinMendieta <kekox.dev@gmail.com>
2023-12-11 15:17:46 -0500

    Add solution to problem #9

---
+++ src/2023/09/switchTheLights.ts
@@ -0,0 +1,21 @@
+export function adjustLights(lights: string[]): number {
+	const lightsMap = new Map<string, string>();
+	lightsMap.set("🟢", "🔴");
+	lightsMap.set("🔴", "🟢");
+
+	const evenLight = lights[0];
+	const oddLight = lightsMap.get(evenLight);
+
+	let adjustingLightsCount = 0;
+	for (let index = 0; index < lights.length; index++) {
+		const light = lights[index];
+		if (
+			(index % 2 === 0 && light === evenLight) ||
+			(index % 2 !== 0 && light === oddLight)
+		) {
+			continue;
+		}
+		adjustingLightsCount++;
+	}
+	return adjustingLightsCount;
+}
