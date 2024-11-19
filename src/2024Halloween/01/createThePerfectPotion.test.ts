d254f86e6ed3b60104e8a684b5b21a0f2d32a0f6
kevin-mendieta-deel <kevin.mendieta@deel.com>
2024-11-18 17:31:00 -0800

    HW2024-1: Add solution

--- src/2024Halloween/01/createThePerfectPotion.test.ts
+++ src/2024Halloween/01/createThePerfectPotion.test.ts
@@ -5,35 +5,35 @@ import { createMagicPotion } from "./createThePerfectPotion";
 describe("first repeated gift problem tests", () => {
   it("should pass test 1", () => {
     const result = createMagicPotion([13, 5, 6, 2], 8);
-    expect(result).toBe([2, 3]);
+    expect(result).toStrictEqual([2, 3]);
   });
   it("should pass test 2", () => {
     const result = createMagicPotion([11, 3, 7, 5], 10);
-    expect(result).toBe([1, 2]);
+    expect(result).toStrictEqual([1, 2]);
   });
   it("should pass test 3", () => {
     const result = createMagicPotion([4, 3, 2, 3, 4], 6);
-    expect(result).toBe([0, 2]);
+    expect(result).toStrictEqual([0, 2]);
   });
   it("should pass test 4", () => {
     const result = createMagicPotion([10, 5, 2, 3, 7, 5], 10);
-    expect(result).toBe([3, 4]);
+    expect(result).toStrictEqual([3, 4]);
   });
   it("should pass test 5", () => {
     const result = createMagicPotion([1, 2, 3, 4, 5], 8);
-    expect(result).toBe([2, 4]);
+    expect(result).toStrictEqual([2, 4]);
   });
   it("should pass test 6", () => {
     const result = createMagicPotion([5, 1, 4, 6, 2], 9);
-    expect(result).toBe([0, 2]);
+    expect(result).toStrictEqual([0, 2]);
   });
   it("should pass test 7", () => {
     const result = createMagicPotion([1, -2, 3, -4, 5], -1);
-    expect(result).toBe([0, 1]);
+    expect(result).toStrictEqual([0, 1]);
   });
   it("should pass test 8", () => {
     const result = createMagicPotion([10, 20, 30, 40], 50);
-    expect(result).toBe([1, 2]);
+    expect(result).toStrictEqual([1, 2]);
   });
   it("should pass test 9", () => {
     const result = createMagicPotion([], 10);
--- src/2024Halloween/01/createThePerfectPotion.ts
+++ src/2024Halloween/01/createThePerfectPotion.ts
@@ -2,5 +2,21 @@ export function createMagicPotion(
   potions: number[],
   target: number,
 ): number[] | undefined {
-  return [];
+  const combinations: Record<string, number[]> = {};
+  for (let i = 0; i < potions.length - 1; i++) {
+    for (let j = i + 1; j < potions.length; j++) {
+      const totalPower = potions[i] + potions[j];
+      const difference = Math.abs(i - j);
+      if (target - totalPower === 0) {
+        if (!combinations[difference]) {
+          combinations[difference] = [i, j];
+        }
+      }
+    }
+  }
+  const closestCombinationIndex: string | undefined =
+    Object.keys(combinations)[0];
+  return closestCombinationIndex
+    ? combinations[closestCombinationIndex]
+    : undefined;
 }
