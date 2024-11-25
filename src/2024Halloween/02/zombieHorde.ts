8225e149d322ec9746803d35fcf196c6aa55ed40
kevin-mendieta-deel <kevin.mendieta@deel.com>
2024-11-25 07:47:15 -0500

    HW2024-2: Improve solution

--- src/2024Halloween/02/zombieHorde.ts
+++ src/2024Halloween/02/zombieHorde.ts
@@ -1,13 +1,8 @@
 export function battleHorde(zombies: string, humans: string): string {
-  let zCount = 0;
-  let hCount = 0;
+  let diff = 0;
   for (let i = 0; i < zombies.length; i++) {
-    zCount += +zombies[i];
-    hCount += +humans[i];
+    diff += +zombies[i] - +humans[i];
   }
-
-  const diff = zCount - hCount;
-
   if (diff === 0) return "x";
   if (diff < 0) return `${-diff}h`;
   return `${diff}z`;
