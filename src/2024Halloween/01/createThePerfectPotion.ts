95492d0835b1db8581273b2d3d0336289d3e652c
kevin-mendieta-deel <kevin.mendieta@deel.com>
2024-11-25 07:46:22 -0500

    HW2024-1: Improve solution

--- src/2024Halloween/01/createThePerfectPotion.ts
+++ src/2024Halloween/01/createThePerfectPotion.ts
@@ -2,21 +2,17 @@ export function createMagicPotion(
   potions: number[],
   target: number,
 ): number[] | undefined {
-  const combinations: Record<string, number[]> = {};
-  for (let i = 0; i < potions.length - 1; i++) {
-    for (let j = i + 1; j < potions.length; j++) {
-      const totalPower = potions[i] + potions[j];
-      const difference = Math.abs(i - j);
-      if (target - totalPower === 0) {
-        if (!combinations[difference]) {
-          combinations[difference] = [i, j];
-        }
-      }
+  const combinations = new Map<number, number>();
+  for (let i = 0; i < potions.length; i++) {
+    const potion = potions[i];
+    const difference = target - potion;
+    const combinationDifference = combinations.get(difference);
+    if (combinationDifference !== undefined) {
+      return [combinationDifference, i];
+    }
+    if (combinationDifference === undefined) {
+      combinations.set(potion, i);
     }
   }
-  const closestCombinationIndex: string | undefined =
-    Object.keys(combinations)[0];
-  return closestCombinationIndex
-    ? combinations[closestCombinationIndex]
-    : undefined;
+  return undefined;
 }
